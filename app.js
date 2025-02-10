27bd42e8367c9c15327241c0d4e873ee3d12427c
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-10 15:22:16 +0100

    create middleware function

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
+// importiamo il middleware di gestione errore server
+const errorsHandler = require("./middlewares/errorsHandler");
+
+// importiamo il middleware di gestione errore 404
+const notFound = require("./middlewares/notFound");
+
 // definiamo la rotta home
 app.get('/', (req, res) => {
     res.send("Ciao sono la rotta Home, della mia pizzeria!!!");
@@ -19,6 +25,11 @@ app.get('/', (req, res) => {
 // utilizziamo la rotta delle pizze andando a definire la parte iniziale delle rotte
 app.use("/posts", postsRouter)
 
+// utilizzo middleware di gestione errore server
+app.use(errorsHandler);
+
+// utilizzo middleware di gestione not found 404
+app.use(notFound);
 
 // avvio del server sulla porta specificata
 app.listen(port, () => {
